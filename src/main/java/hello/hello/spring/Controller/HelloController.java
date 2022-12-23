52784b9babaddca9a44cbe9ad91ac5f3e60c6fb7
LDY <leedy3838@naver.com>
2022-12-23 20:15:58 +0900

    API를 이용해서 데이터 웹 페이지에 출력하기

--- src/main/java/hello/hello/spring/Controller/HelloController.java
+++ src/main/java/hello/hello/spring/Controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,6 +18,33 @@ public class HelloController {
     @GetMapping("hello-mvc")
     public String helloMVE(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
-        return "hello-template";
+        return "hello_template";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api-json")
+    @ResponseBody
+    public Data helloJson(@RequestParam("name") String name){
+        Data data = new Data();
+        data.setName(name);
+
+        return data;
+    }
+
+    static class Data{
+        String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
