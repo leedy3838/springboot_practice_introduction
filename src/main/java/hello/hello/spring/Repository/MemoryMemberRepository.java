18ae4bc28a9abe724c6d97f523c021fcd293e97a
LDY <leedy3838@naver.com>
2022-12-24 13:09:26 +0900

    회원 도메인과 레포지토리 만들기

---
+++ src/main/java/hello/hello/spring/Repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hello.spring.Repository;
+
+import hello.hello.spring.Domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    //sequence는 id를 생성하는 역할을 할 것임.
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
