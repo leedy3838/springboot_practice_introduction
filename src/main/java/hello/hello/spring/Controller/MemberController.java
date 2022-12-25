bd171de769abf45a6843ce4ba2974164d325f4a2
LDY <leedy3838@naver.com>
2022-12-25 14:54:30 +0900

    회원 웹 기능 - 회원 가입

--- src/main/java/hello/hello/spring/Controller/MemberController.java
+++ src/main/java/hello/hello/spring/Controller/MemberController.java
@@ -1,8 +1,12 @@
 package hello.hello.spring.Controller;
 
+import hello.hello.spring.Domain.Member;
 import hello.hello.spring.Service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +15,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
